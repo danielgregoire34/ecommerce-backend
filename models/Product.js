28e5aa536ddf447f2e74aeedbb81a22193c9c287
danielgregoire34 <danielgregoire34@gmail.com>
2023-02-25 16:35:34 -0500

    added info to catgory,product,producttag,tag and categoryroutes, fleshing out there ids and information

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,37 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,     
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      //validates that the value is a decimal 
+      validate: {
+        isProduct: true,
+      },
+    },
+    stock:{
+      type: DataTypes.DECIMAL,
+      allowNull:false,
+      defaultValue: 10,
+      //Validates that the value is numeric
+    },
+    category_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
